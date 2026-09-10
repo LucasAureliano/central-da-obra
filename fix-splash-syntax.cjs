8b6976c14a28c88b7aadcadc39685fadf3f568ea
Lucas <lucas@centraldaobra.com>
2026-09-10 19:46:08 -0300

    fix: App.tsx syntax error from splash screen hide

---
+++ fix-splash-syntax.cjs
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let code = fs.readFileSync('src/App.tsx', 'utf8');
+
+code = code.replace(/    \/\/ APP ENTRANCE SPLASH SCREEN\n      return <SplashScreen onComplete=\{\(\) => \{/g, "    // APP ENTRANCE SPLASH SCREEN\n    if (!hasShownAppSplash) {\n      return <SplashScreen onComplete={() => {");
+
+fs.writeFileSync('src/App.tsx', code, 'utf8');
+console.log("Fixed App.tsx syntax");
