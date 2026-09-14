b5dd220e89471d35a4bdc5439e015a97f425982e
Lucas <lucas@centraldaobra.com>
2026-09-14 14:50:12 -0300

    fix: remove scratch files

--- plan-works.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-
-let code = fs.readFileSync('src/components/Works.tsx', 'utf8');
-
-// 1. We have `const filteredWorks = works.filter...`
-// But we also have `localFilteredWorks` already created by me earlier (Wait, did I create `localFilteredWorks` in Works.tsx? Let me check)
--- update-types.cjs
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('src/types.ts', 'utf8');
-code = code.replace(/userId\?: string;/g, "userId?: string;\n  client?: string;\n  providerName?: string;\n  colorTheme?: string;\n  order?: number;");
-fs.writeFileSync('src/types.ts', code, 'utf8');
-console.log("Updated types.ts");
